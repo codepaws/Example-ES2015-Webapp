de145a636a37c9bdc6f01670123431a4ac470e37
brodie <brodiesmith@gmail.com>
2017-02-19 16:29:05 +0800

    refactored common code

--- src/post.js
+++ src/post.js
@@ -1,21 +1,8 @@
+import API from "./api"
+
 let Post = {
   findAll() {
-    return new Promise( (resolve, reject) =>  {
-      let uri = "http://localhost:3000/posts";
-      let request = new XMLHttpRequest();
-
-      request.open("GET", uri, true) // last option is if this call should be performed async
-      request.onload = () => {
-        if(request.status >= 200 && request.status < 400) {
-          resolve( JSON.parse(request.response));
-        }
-      }
-
-      request.onerror = () => {
-        reject( new Error("Something went wrong with the API"));
-      }
-      request.send();
-    });
+    return API.fetch("posts");
   }
 }
 
